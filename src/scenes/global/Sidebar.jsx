0e7eaf2072ffd560403e996999bc04260c2f5132
KartikeyDiyewar <diyewarkartikey@gmail.com>
2024-09-05 17:18:56 +0530

    fixed Sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -47,6 +47,7 @@ const Sidebar = () => {
       sx={{
         "& .pro-sidebar-inner": {
           background: `${colors.primary[400]} !important`,
+          position: "fixed",
         },
         "& .pro-icon-wrapper": {
           backgroundColor: "transparent !important",
